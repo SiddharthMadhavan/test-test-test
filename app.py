accf51ecc855dfe64ff468d7cf108ec2818ff1b4
Siddharth Madhavan <79624807+SiddharthMadhavan@users.noreply.github.com>
2026-01-21 03:35:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from qdrant_client.models import Filter, FieldCondition, MatchValue
 from qdrant_client.models import PayloadSchemaType
 import numpy as np
 from datetime import datetime
-from google-generativeai import genai
+import google.generativeai as genai
 import os
 
 app = Flask(__name__)
